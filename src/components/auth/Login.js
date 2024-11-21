14a3ad6719a26eae775527f0d301b4f865cd2805
mila_zagrevskaya <mila.zagrevskaya@gmail.com>
2024-11-21 17:01:18 +0100

    "Refactor Header component to use functional component and update navigation, fix logout functionality, and update styles in Home component and Login component"

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import firebaseConfig, { auth } from 'config/firebaseConfig';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from 'config/firebaseConfig';
 import { Link, useNavigate } from 'react-router-dom';
